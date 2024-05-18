3ddf1b50da47982323e8bc658af64e219440fcc0
Isidora <isidora.oyanedel@usach.cl>
2024-05-17 21:09:04 -0400

    wea bien

--- gateway-service/src/test/java/com/example/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/example/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.gatewayserver;
+package com.example.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServerApplicationTests {
+class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
